4ddd1f0c1c48c5a3ca191b9506203b34f41e926c
tatyana_belova_02 <tatyana_belova_02@mail.ru>
2022-12-22 12:15:04 +0300

    Calculator

---
+++ calcul.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	double firstValue, secondValue;
+	char operation;
+	cout << "Enter first value" << endl;
+	cin >> firstValue;
+
+	cout << "Enter second value" << endl;
+	cin >> secondValue;
+
+	cout << "Enter operation value" << endl;
+	cin >> operation;
+
+	switch (operation)
+	{
+	case '+':
+		cout << "Your value:" << firstValue + secondValue << endl;
+		break;
+
+	case '-':
+		cout << "Your value:" << firstValue - secondValue << endl;
+		break;
+
+	case '*':
+		cout << "Your value:" << firstValue * secondValue << endl;
+		break;
+
+	case '/':
+		cout << "Your value:" << firstValue / secondValue << endl;
+		break;
+	}
+	return 0;
+}
+
